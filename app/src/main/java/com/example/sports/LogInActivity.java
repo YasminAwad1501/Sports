c8b4fdbd37b21045798c2f53233c5596883b57ab
yasminawad1501 <yasminawad1501@gmail.com>
2021-10-27 14:28:47 +0300

    27.10

--- app/src/main/java/com/example/sports/LogInActivity.java
+++ app/src/main/java/com/example/sports/LogInActivity.java
@@ -1,38 +1,95 @@
 package com.example.sports;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogInActivity extends AppCompatActivity  {
 
 
+    private static final String TAG = "FIREBASE";
+    //declaring all
     private EditText editTextUsername, editTextPassword;
     private Button buttonLogIn, buttonSignUp;
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        //returns reference to the instance of the project Firebase
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
     public void login(View view) {
-        Intent intent = new Intent(this, SportsActivity.class);
-        startActivity(intent);
+        if (!editTextUsername.getText().toString().equals(""))
+        {
+
+            //saving email and password of user in local  file for future use
+            //create sp file
+            SharedPreferences sp = getSharedPreferences("settings",MODE_PRIVATE);
+            //open editor for editing
+            SharedPreferences.Editor editor = sp.edit();
+            //write the wanted settings
+            editor.putString("email", editTextUsername.getText().toString());
+            editor.putString("password", editTextPassword.getText().toString());
+            //save and close file
+            editor.commit();
+
+
+            login(editTextUsername.getText().toString(), editTextPassword.getText().toString());
+        }
+
     }
 
 
     public void Signup(View view) {
+
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
+
     }
+
+    public void login(String email,String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(LogInActivity.this, SportsActivity.class);
+                            startActivity(i);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LogInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 
 
 }
--- app/src/main/java/com/example/sports/SignUpActivity.java
+++ app/src/main/java/com/example/sports/SignUpActivity.java
@@ -1,26 +1,70 @@
 package com.example.sports;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private static final String TAG = "FIREBASE";
     private EditText editTextName, editTextUsername, editTextPassword, editTextEmail, editTextBirthday, editTextPhoneNumber;
     private Button buttonSignUp;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void Signup(View view) {
         Intent intent = new Intent(this, SportsActivity.class);
         startActivity(intent);
     }
+
+    public void signup(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent (SignUpActivity.this, SportsActivity.class);
+                            startActivity(i);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void submit(View view){
+        signup(editTextUsername.getText().toString(), editTextPassword.getText().toString());
+    }
+
+
 }
