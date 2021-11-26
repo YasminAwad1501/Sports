de88e6cb8fff2a4f4a5f7252dd7ef79e48217c8b
yasminawad1501 <yasminawad1501@gmail.com>
2021-11-26 08:43:39 +0200

    26.11

---
+++ app/src/main/java/com/example/sports/NotificationReceiver.java
@@ -0,0 +1,13 @@
+package com.example.sports;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
